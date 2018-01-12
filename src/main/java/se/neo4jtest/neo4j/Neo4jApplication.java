9d0c5abafb0b91260e65856af765e1cfc98ef0b1
Johan Wiström <johan.wistrom@reforceinternational.com>
2018-01-12 16:31:04 +0100

    init

---
+++ src/main/java/se/neo4jtest/neo4j/Neo4jApplication.java
@@ -0,0 +1,12 @@
+package se.neo4jtest.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jApplication.class, args);
+	}
+}
